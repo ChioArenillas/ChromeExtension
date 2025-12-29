10701275d3a4b6650640906161eb6c8db95d930e
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-29 11:12:51 +0100

    new

--- index.js
+++ index.js
@@ -5,25 +5,31 @@
 //     console.log("Button clicked from onclick attribute")
 // }
  
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
+function renderLeads(){
+let listItems= ""
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-
-    //OTRA FORMA DE HACERLO:
-    //const li = document.createElement("li") - Crear el elemento
-    //li.textContent = myLeads[i] - set textcontent
-    //ulEl.append(li) - append to ul
+    listItems += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+    </li>`
+}
+ulEl.innerHTML= listItems //haciendo fuera del for el .innerHTML solo lo usas una vez, si estuviera dentro del loop se usaría 3 veces, como el uso del DOM tiene un coste, es mejor usarlo solo una vez
 }
 
 
+ 
 
 
