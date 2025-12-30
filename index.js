c202b9d678fa0b059abaea66ae4c56cc21fdf447
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-30 16:45:40 +0100

    new

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: "https://leads-trancker-app-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+console.log(database)
 
 let myLeads = []
 const inputEl = document.getElementById("input-el")
